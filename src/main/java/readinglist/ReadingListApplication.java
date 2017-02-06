20cfeca12c1561d3f1ee774b2850f1e22d023f6f
Nemanja Vuckovic <nemanja.vuckovic@farncombe.com>
2017-02-06 21:02:55 +0100

    WOW

--- src/main/java/readinglist/ReadingListApplication.java
+++ src/main/java/readinglist/ReadingListApplication.java
@@ -3,6 +3,8 @@ package readinglist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// WOW
 //u browseru otvaras: http://localhost:8080/readingList/pera
 @SpringBootApplication	//Enable component-scanning and auto configuration
 public class ReadingListApplication {
