af9ced3dd614211ea7a9eda726a27de7dc4d3652
Nemanja Vuckovic <nemanjav@ccbilleu.com>
2017-05-23 11:00:26 +0200

    Added delete book entity func to readingList.html

--- src/main/java/readinglist/SecurityConfig.java
+++ src/main/java/readinglist/SecurityConfig.java
@@ -2,7 +2,6 @@ package readinglist;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
